dd8ceeb9a2e53a711a208481c421feca942d161c
Nguyen Huu Hiep <nhhiepit@gmail.com>
2023-12-28 08:49:34 +0700

    Bai 1

---
+++ bai1/demo.sql
@@ -0,0 +1,22 @@
+create database demo;
+
+use demo;
+
+create table Students (
+	id int,
+    name varchar(200),
+    age int,
+    country varchar(50)
+);
+
+create table Classs (
+	id int,
+    name varchar(50)
+);
+
+create table Teachers (
+	id int,
+    name varchar(50),
+    age int,
+    country varchar(100)
+);
